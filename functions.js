b9269103f49690742531f6592f776ef61a3eba12
Mohd Amaan <amaansmarty1010@gmail.com>
2026-06-24 13:02:29 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,30 @@
+function sayMyName(){
+console.log("A");
+console.log("m");
+console.log("a");
+console.log("a");
+console.log("n");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1  + number2);
+// }
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(3,5)
+
+// console.log("Result:",result);
+
+function loginUserMessage(username = "sam"){
+    if(!username ){
+        console.log("please enter a username ");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("Amaan"))
